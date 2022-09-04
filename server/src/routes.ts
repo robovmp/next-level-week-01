d57e5b5da5c900580746f2add1c63bda00201bfc
robovmp <mpviniciusmp@gmail.com>
2022-09-04 17:47:08 -0300

    fix: adicionando validações

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/Points';
 import ItemsController from './controllers/Items';
@@ -14,8 +15,26 @@ const itemsController =  new ItemsController();
 
 routes.get( '/items', itemsController.index );
 
-routes.post( '/points', upload.single( 'image' ), pointsController.create );
 routes.get( '/points', pointsController.index );
 routes.get( '/points/:id', pointsController.show );
+
+routes.post( 
+    '/points', 
+    upload.single( 'image' ),
+    celebrate( {
+        body: Joi.object().keys( {
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        } )
+    }, { abortEarly: false }
+    ), 
+    pointsController.create 
+);
 
 export default routes;
